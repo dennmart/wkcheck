f036a5debcb7c5ec1796477de89cf080b42a094f
Dennis Martinez <dennis@dennmart.com>
2013-05-17 16:57:52 -0700

    Fix tests for Minitest included in Ruby 1.9.2

--- test/wkcheck_test.rb
+++ test/wkcheck_test.rb
@@ -6,8 +6,6 @@ require 'wkcheck'
 
 describe WKCheck::Stats do
   describe ".study_queue" do
-    let(:stats) { WKCheck::Stats.new }
-
     def study_queue_data(lessons, reviews)
       {
         "lessons_available"           => lessons,
@@ -18,10 +16,12 @@ describe WKCheck::Stats do
       }
     end
 
+    subject { WKCheck::Stats.new }
+
     it "displays pending lessons and reviews" do
       Wanikani::StudyQueue.expects(:queue).returns(study_queue_data(10, 500))
 
-      message = stats.study_queue
+      message = subject.study_queue
       message.must_match /You have 10 lessons pending/
       message.must_match /You have 500 reviews pending/
     end
@@ -29,14 +29,14 @@ describe WKCheck::Stats do
     it "uses the word 'no' instead of '0' if there are zero lessons or reviews" do
       Wanikani::StudyQueue.expects(:queue).returns(study_queue_data(10, 0))
 
-      message = stats.study_queue
+      message = subject.study_queue
       message.must_match /You have no reviews pending/
     end
 
     it "displays the next review date if there are zero lessons and reviews" do
       Wanikani::StudyQueue.expects(:queue).returns(study_queue_data(0, 0))
 
-      message = stats.study_queue
+      message = subject.study_queue
       message.must_equal "You have no lessons or reviews now! You'll have some more on Tuesday, January 1 at 8:00 AM."
     end
   end
