f42eaa915fc3aafc5c4e7c44c1564595b597e18c
Dennis Martinez <dennis@dennmart.com>
2014-04-11 10:47:25 -0700

    Display error message instead of blowing up on failure

--- test/wkcheck_run.rb
+++ test/wkcheck_run.rb
@@ -1,19 +1,48 @@
 # wkcheck integration test with real calls.
 require 'rainbow'
 
+# Help message
 print "Running #{Rainbow('wkcheck -h').yellow.bright}... "
-`wkcheck -h`
-puts Rainbow("GOOD!").green.bright
+begin
+  `wkcheck -h`
+  puts Rainbow("GOOD!").green.bright
+rescue
+  puts Rainbow("ERROR!").red.bright
+  error = true
+end
 
+# Version display
 print "Running #{Rainbow('wkcheck -v').yellow.bright}... "
-`wkcheck -v`
-puts Rainbow("GOOD!").green.bright
+begin
+  `wkcheck -v`
+  puts Rainbow("GOOD!").green.bright
+rescue
+  puts Rainbow("ERROR!").red.bright
+  error = true
+end
 
+# Study Queue
 print "Running #{Rainbow('wkcheck -q').yellow.bright}... "
-`wkcheck -q`
-puts Rainbow("GOOD!").green.bright
+begin
+  `wkcheck -q`
+  puts Rainbow("GOOD!").green.bright
+rescue
+  puts Rainbow("ERROR!").red.bright
+  error = true
+end
 
+# Level Progression
 print "Running #{Rainbow('wkcheck -l').yellow.bright}... "
-`wkcheck -l`
-puts Rainbow("GOOD!").green.bright
+begin
+  `wkcheck -l`
+  puts Rainbow("GOOD!").green.bright
+rescue
+  puts Rainbow("ERROR!").red.bright
+  error = true
+end
 
+if error
+  puts Rainbow("There was an error on one or more commands - please run the commands manually to see what happened...").red
+else
+  puts Rainbow("All commands are green, you're good to go!").green
+end
