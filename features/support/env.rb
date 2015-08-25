d02863fd90f8e7c03691968e9349a09b72ed45ba
Dennis Martinez <dennis@dennmart.com>
2015-08-25 09:37:24 -0700

    Fixed errors for Cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,8 @@
 require 'aruba/cucumber'
+require 'mocha/api'
 
 # Set the bin directory in PATH so we can easily execute it.
 ENV['PATH'] = File.expand_path('../../bin', File.dirname(__FILE__)) + File::PATH_SEPARATOR + ENV['PATH']
 
-Before do
-  # Since we save the .wkcheck.yml file in the home directory,
-  # let's temporarily set the home directory to /tmp.
-  @original_homedir = ENV['HOME']
-  tmp_homedir = File.join('/tmp')
-  FileUtils.rm_rf(tmp_homedir, :secure => true)
-  ENV['HOME'] = tmp_homedir
-end
-
-After do
-  # Revert the temporary home directory.
-  ENV['HOME'] = @original_homedir
-end
+# Set the constant for the config file path to /tmp for the test run.
+WKCheck.const_set("CONFIG_FILE", "/tmp")
